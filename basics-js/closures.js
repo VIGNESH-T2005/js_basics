b8a92dcc0feb2eb6e6a4a37f0e0507d3baffb63e
VIGNESH-T2005 <vigneshhari8739@gmail.com>
2026-06-06 00:05:15 +0530

    closures

---
+++ basics-js/closures.js
@@ -0,0 +1,14 @@
+// closures
+// A closure is a function that has access to its own scope, the outer function's scope, and the global scope.
+// A closure is created when a function is defined inside another function
+//  and the inner function has access to the outer function's variables.
+function outerFunction() {
+    let outerVariable = "vicky";
+    function innerFunction() {
+        console.log(outerVariable);
+    }
+    return innerFunction;
+}
+const closureFunction = outerFunction();
+closureFunction(); 
+
