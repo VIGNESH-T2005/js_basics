7075a1e4b704b654e7eabf46b46e09f74acf6c4c
VIGNESH-T2005 <vigneshhari8739@gmail.com>
2026-06-05 23:50:15 +0530

    maps in array

---
+++ basics-js/arrays/map.js
@@ -0,0 +1,13 @@
+// maps in arrays
+
+const arr1 = [1, 2, 3];
+const mappedArr = arr1.map((num) => num * 2);
+console.log(mappedArr);
+
+const arr2= ["apple", "banana", "cherry"];
+const mappedArr2 = arr2.map((fruit) => fruit.toUpperCase());
+console.log(mappedArr2);
+
+const arr3 = [1, 2, 3];
+const mappedArr3 = arr3.map((num, index) => num + index);
+console.log(mappedArr3);
