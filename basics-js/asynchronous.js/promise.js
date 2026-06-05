eab20b960df95cd8b7829d3655ea5f57bc13d6c2
VIGNESH-T2005 <vigneshhari8739@gmail.com>
2026-06-05 23:46:33 +0530

    promises in built

--- basics-js/asynchronous.js/promise.js
+++ basics-js/asynchronous.js/promise.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const axios = require('axios');
 //Promises 
 // new promise((resolve,reject)=>{condition?resolve():reject()}) 
 // .then(()=>{success code}).catch(()=>{failure code});
@@ -31,4 +33,11 @@ const newPromise= new Promise((resolve,reject)=>{
 newPromise.then(()=>{console.log('success')}).catch(()=>{console.log('failure')});
 
 // in built promises
+const url = 'https://jsonplaceholder.typicode.com/users/';
+fs.promises.readFile('txt1.txt','utf-8')
+.then((data)=>{console.log(data)})
+.catch((err)=>{console.log(err)});
 
+axios.get(url+ 1)
+.then((response)=>{console.log(response.data);})
+.catch((error)=>{console.log(error)});   
